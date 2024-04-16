3e77153c32e583fc04a450fa9db03f0287e9915e
ManuA7X <44942727+ManuA7X@users.noreply.github.com>
2024-04-16 17:40:27 +0200

    factorial

--- PracticaAlgoritmos/src/util/Algoritmos.java
+++ PracticaAlgoritmos/src/util/Algoritmos.java
@@ -2,4 +2,27 @@ package util;
 
 public abstract class Algoritmos {
 
+	/**
+     * Calcula la sucesion de Fibonacci de un numero dado
+     *
+     * @param numero 		El numero del cual se calculara la sucesion de Fibonacci
+     * @return 				La sucesion de Fibonacci correspondiente al numero dado
+     */
+    public static int fibonacci(int numero) {
+        if (numero <= 1)
+            return numero;
+        return fibonacci(numero - 1) + fibonacci(numero - 2);
+    }
+    
+    /**
+     * Calcula el factorial de un numero dado
+     *
+     * @param numero 		El numero del cual se calculara el factorial
+     * @return 				El factorial del numero dado
+     */
+    public static int factorial(int numero) {
+        if (numero == 0)
+            return 1;
+        return numero * factorial(numero - 1);
+    }
 }
